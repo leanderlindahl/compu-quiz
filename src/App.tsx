258fec440f3cb0c755705951878d5000457c4e54
Leander Lindahl <leanderlindahl@gmail.com>
2020-01-23 13:20:05 +0100

    Add fetchQuestions to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import htmlDecode from './helpers/html-decode';
+
 import './App.css';
 
+interface QuizData {
+  response_code: number;
+  results: QuizQuestion[];
+}
+interface QuizQuestion {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string;
+}
+
 const App: React.FC = () => {
+  async function fetchQuestions(): Promise<QuizData> {
+    const requestUrl = `${process.env.REACT_APP_API_URL}?amount=${process.env.REACT_APP_NUMBER_OF_QUESTIONS}&category=${process.env.REACT_APP_CATEGORY}&difficulty=easy`;
+    const response = await fetch(requestUrl);
+    const data = await response.json();
+    return data;
+  }
+
+  const [questions, setQuestions] = useState<QuizQuestion[]>([]);
+
+  useEffect(() => {
+    fetchQuestions()
+      .then(data => setQuestions(data.results))
+      .catch(err => console.warn(err));
+  }, []);
+
+  const questionsList = questions.map(item => (
+    <li key={item.question} className="questions_list-item">
+      {htmlDecode(item.question)}
+    </li>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Computer Quiz.</h1>
       </header>
+      <main>{questionsList.length <= 0 ? 'Loading Questions...' : questionsList}</main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/helpers/html-decode.js
@@ -0,0 +1,7 @@
+function htmlDecode(input) {
+  const e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue;
+}
+
+export default htmlDecode;
